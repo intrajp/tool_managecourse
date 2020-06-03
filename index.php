6705fbf14a78d207d287b14473f93ae7fed6d271
fujiwara <shintaro.fujiwara@gmail.com>
2020-06-03 20:21:32 +0900

    fix bug in couting courses.

--- index.php
+++ index.php
@@ -79,8 +79,10 @@ foreach ($columns as $column=>$strcolumn) {
 }
 
 $baseurl = new moodle_url('index.php', array('sort' => $sort, 'dir' => $dir, 'perpage' => $perpage));
-echo "Showing ".$renderer->show_table2_count()." courses which has manager, coursecreator, editingteacher or teacher role.";
-echo $OUTPUT->paging_bar($renderer->show_table2_count(), $page, $perpage, $baseurl);
+$count2_actual = $renderer->show_table2_count();
+$count2_redundant = $renderer->show_table2_count_redundant();
+echo "Showing ".$count2_actual."(".$count2_redundant." redundant) courses which has manager, coursecreator, editingteacher or teacher role.";
+echo $OUTPUT->paging_bar($count2_redundant, $page, $perpage, $baseurl);
 echo $renderer->show_table2($page, $perpage);
 
 echo "<a href=\"course_file_size.php\">course file size</a>";
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_managecourse';
-$plugin->release = '0.1.11';
-$plugin->version = 2020060305;
+$plugin->release = '0.1.12';
+$plugin->version = 2020060306;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_ALPHA;
