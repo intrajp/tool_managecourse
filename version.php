a5bc2ec53f84cdd777bc1b6412c4726651c83d38
fujiwara <shintaro.fujiwara@gmail.com>
2020-06-17 03:10:06 +0900

    new version.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_managecourse';
-$plugin->release = '0.1.41';
-$plugin->version = 2020061723;
+$plugin->release = '0.1.42';
+$plugin->version = 2020061724;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_ALPHA;
